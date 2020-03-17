581afe55ec1edebe3faa9953887359cadb2cfdf6
corbie1 <bratchikovjohn@gmail.com>
2020-03-17 14:18:26 +0300

    1
    1

--- js/index.js
+++ js/index.js
@@ -89,6 +89,7 @@ jQuery(function($){
     infinite: true,
     useTransform: true,
     speed: 400,
+    variableWidth: true,
     cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
  });
 
@@ -102,25 +103,7 @@ jQuery(function($){
         dots: false,
         focusOnSelect: false,
         infinite: false,
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 5,
-            }
-        }, {
-            breakpoint: 640,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 4,
-            }
-        }, {
-            breakpoint: 420,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-        }
-        }]
+   
     });
 
  $('.slider-single').on('afterChange', function(event, slick, currentSlide) {
